cc3ab1f5893513e151f4832a1802989efb86b767
gergelyHorvath <gergely.horvath126@gmail.com>
2017-04-25 00:41:46 +0200

    sudoku solver slow but steady ready

--- Sudoku.py
+++ Sudoku.py
@@ -1,25 +1,20 @@
 import copy
 
+
 def solve(board, d=1):
-    mi = most_information(board)
-    if not mi:
+    inf = most_information(board)
+    if not inf:
         return board
-    for (r, c) in mi:
-        possib = valid_nums(board, r, c)
-        print(possib)
-        #print(d)
-        if not possib:
-            return
-        for v in possib:
+    if inf == 'STOP':
+        return
+    for idx in range(len(inf)):
+        r, c = inf[idx][0], inf[idx][1]
+        for v in inf[idx][2]:
             new_board = copy.deepcopy(board)
             new_board[r][c] = v
-            print(new_board)
             result = solve(new_board, d+1)
             if result:
                 return result
-
-
-
 
 
 def list_gen(board, r, c):
@@ -34,22 +29,17 @@ def most_information(board):
     inf = []
     for r in range(9):
         for c in range(9):
-            if board[r][c]:
-                inf.append(0)
-            else:
-                all_inf = (
-                    len([i for i in board[r] if i]) +
-                    len([j[c] for j in board if j[c]]) +
-                    len([k for k in list_gen(board, r, c) if k])
-                    )
-                inf.append(all_inf)
-    inf = sorted([i for i in enumerate(inf) if i[1]], key=lambda x: x[1], reverse=True)
-    inf = [(i[0] // 9, i[0] % 9) for i in inf]
+            if not board[r][c]:
+                data = [r, c, valid_nums(board, r, c)]
+                if len(data[2]) == 0:
+                    return 'STOP'
+                inf.append(data)
+    inf = sorted(inf, key=lambda x: len(x[2]))
     return inf
 
 
 def valid_nums(board, r, c):
-    nums = list(set(range(1, 9)) - (
+    nums = list(set(range(1, 10)) - (
         set(board[r]) |
         set([j[c] for j in board]) |
         set(list_gen(board, r, c))
@@ -71,19 +61,8 @@ puzzle = [[5,3,0,0,7,0,0,0,0],
 
 #print(most_information(puzzle))
 #print(list_gen(puzzle, 5, 5))
-#print(valid_nums(puzzle, 0, 2))
-#solve(puzzle)
+#print(valid_nums(puzzle, 5, 3))
 
-puzzle2=[[5,3,0,0,7,0,0,0,0],
-          [6,0,0,1,9,5,0,0,0],
-          [0,9,8,0,0,0,0,6,0],
-          [8,0,0,0,6,0,0,0,3],
-          [4,0,0,8,0,3,0,0,1],
-          [7,0,0,0,2,0,0,0,6],
-          [0,6,0,0,0,0,2,8,0],
-          [0,0,0,4,1,9,0,0,5],
-          [0,0,0,0,8,0,0,7,9]]
-
-solve(puzzle2)
+print(solve(puzzle))
     
 
--- test.py
+++ test.py
@@ -1,1 +1,19 @@
-print(bool([0]))
+def most_information(board):
+    inf = []
+    for r in range(9):
+        for c in range(9):
+            if board[r][c]:
+                inf.append(0)
+            else:
+                all_inf = (
+                    len([i for i in board[r] if i]) +
+                    len([j[c] for j in board if j[c]]) +
+                    list_gen(board, r, c)[1]
+                    )
+                inf.append(all_inf)
+    inf = sorted([i for i in enumerate(inf) if i[1]], key=lambda x: x[1], reverse=True)
+    inf = [(i[0] // 9, i[0] % 9) for i in inf]
+    #inf = sorted(inf, key=lambda x: valid_nums(board, x[0], x[1]), reverse=True)
+    for i in inf:
+        print(len(valid_nums(board, i[0], i[1])))
+    return inf
